aa8d2a5d7c664523d3371dd697732ad2ee968445
Vladimir <KazariaF@yandex.ru>
2023-07-19 23:42:10 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FilmorateApplication {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         SpringApplication.run(FilmorateApplication.class, args);
     }
 
