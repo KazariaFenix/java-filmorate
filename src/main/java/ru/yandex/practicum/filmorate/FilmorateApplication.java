f8d783fc5bfd8820607839724f0d1c81dfbc8f17
Vladimir <KazariaF@yandex.ru>
2023-07-19 23:19:23 +0300

    fix: доработка метода

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmorateApplication {//для логирования использовал внутренние средства Spring Framework
+public class FilmorateApplication {
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
     }
