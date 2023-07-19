7dc399fbfd34a6b748c507aaa2f43405ce1e5904
Vladimir <KazariaF@yandex.ru>
2023-07-19 23:16:40 +0300

    style: исправление стилистических недочетов

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {//для логирования использовал внутренние средства Spring Framework
-
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
     }
-
 }
